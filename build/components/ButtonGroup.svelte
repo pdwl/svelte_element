880ecddc769020df6180e0d1dfaf5b55b21b3dfd
pdwl <pd87@qq.com>
2022-03-09 00:00:45 +0800

    删除无用的

--- build/components/ButtonGroup.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    import clsx from './../common/clsx/index'
-</script>
-
-<div class="{clsx('el-button-group',$$props['class'])}" style={$$props['style']}>
-    <slot />
-</div>
--- build/components/Main.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    import clsx from './../common/clsx/index'
-</script>
-
-<main class="{clsx('el-main',$$props['class'])}" style={$$props['style']}>
-    <slot />
-</main>
